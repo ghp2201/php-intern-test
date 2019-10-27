93841f07015c399f2e70ed707e53bbb171a7e66e
gustavo hnerique pinto <ghp2201@gmail.com>
2019-10-27 16:12:33 -0300

    inicio da construção do bd e do crud

--- pdo.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $pdo = new PDO('mysql:host = localhost; dbname = products', 'root', '', array(PDO::ATTR_PERSISTENT => true));
-    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo -> exec("SET CHARACTER SET utf8");
-?>
