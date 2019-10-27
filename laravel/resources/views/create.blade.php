95109257da775d3e38bcc410a8e0e55536efa887
gustavo hnerique pinto <ghp2201@gmail.com>
2019-10-27 19:02:16 -0300

    view edit pronta

--- laravel/resources/views/create.blade.php
+++ laravel/resources/views/create.blade.php
@@ -8,9 +8,9 @@
 <form action = "{{ route('produtos.store') }}" method = "POST">
     @csrf
     <div class = "row">
-        Nome: <input type = "text" name = "nome" class = "form-control" palceholder = "Nome do produto">
-        Descrição: <input type = "text" name = "descricao" class = "form-control" palceholder = "Descrição do produto">
-        Valor: <input type = "text" name = "valor" class = "form-control" palceholder = "Valor do produto">
+        Nome: <input type = "text" name = "nome" class = "form-control" placeholder = "Nome do produto">
+        Descrição: <input type = "text" name = "descricao" class = "form-control" placeholder = "Descrição do produto">
+        Valor: <input type = "text" name = "valor" class = "form-control" placeholder = "Valor do produto">
         <button type = "submit" class = "btn btn-primary">Enviar</button>
     </div>
 </form>
